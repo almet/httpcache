1882b5dfb74a3c46588249dd4f1fe07d90544cea
Alexis Métaireau <alexis@notmyidea.org>
2012-10-05 17:25:11 +0200

    Add some comments

--- httpcache/webproxy.py
+++ httpcache/webproxy.py
@@ -5,18 +5,30 @@ from urlparse import urlparse, urljoin
 
 
 class Application(object):
+    """The WSGI Application doing the requests and the caching
+
+    :param distant: the url of the distant server
+    :param cache: the cache object, if any. It should respect
+                  the 'set' and 'get' methods.
+    """
 
     def __init__(self, distant, cache=None):
         self.distant = distant
         self.cache = cache
 
     def __call__(self, env, start_response):
+        """The actual WSGI callable.
+
+        It will do the request if there is the need to, cache it, and otherwise
+        will just return what he knows from the cache.
+        """
         method = env['REQUEST_METHOD']
         path = env['PATH_INFO']
 
         if env['QUERY_STRING']:
             path += '?' + env['QUERY_STRING']
 
+        # todo need to handle headers as well
         return self.proxy(method, path, start_response)
 
     def proxy(self, method, path, start_response):
